326c6f5e1c147f9a1576ca5e6c0eeec06dd6a3ad
Bablu kumar <bk596572@gmail.com>
2025-08-09 14:06:43 +0530

    feat: add Holdings and Positions models, seed initial data, and test API endpoints

---
+++ backend/model/PositionsModel.js
@@ -0,0 +1,4 @@
+const { model } = require("mongoose");
+const { PositionSchema } = require("../schemas/PositionsSchema");
+const PositionsModel = new model("position", PositionSchema);
+module.exports = { PositionsModel };
---
+++ backend/schemas/OrdersSchema.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require("mongoose");
+const OrdersSchema = new Schema({
+  name: String,
+  qty: Number,
+  price: Number,
+  mode: String,
+});
+
+module.exports = { OrdersSchema };
